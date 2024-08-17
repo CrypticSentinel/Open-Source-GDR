5513255a43609440d38ee074ffd4c46c329aae2c
Cryptic Sentinel <140169658+CrypticSentinel@users.noreply.github.com>
2024-08-17 15:58:09 +0200

    Add files via upload

--- Magia Libera/SpellCheck/service-worker.js
+++ Magia Libera/SpellCheck/service-worker.js
@@ -1,15 +1,14 @@
-// service-worker.js
-self.addEventListener('install', event => {
+self.addEventListener('install', (event) => {
   console.log('Service Worker installing.');
 });
 
-self.addEventListener('activate', event => {
-  console.log('Service Worker activated.');
+self.addEventListener('activate', (event) => {
+  console.log('Service Worker activating.');
 });
 
-self.addEventListener('fetch', event => {
+self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request).then(response => {
+    caches.match(event.request).then((response) => {
       return response || fetch(event.request);
     })
   );
